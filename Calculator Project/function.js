93f50622ba8b17fbee86f9d6908b9dd298f576f0
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2021-11-27 03:37:59 +0000

    Added Previous history in the screen :)

--- Calculator Project/function.js
+++ Calculator Project/function.js
@@ -1,34 +1,41 @@
 var buttons = document.getElementsByClassName("button");
-var display = document.getElementById("display");
+var display = document.getElementById("current-display");
+var prev_display = document.getElementById("previous-display");
 
 //
 var operand1 = 0;
 var operand2 = null;
 var operator = null;
-
+var flag=true;
 function isOperator(value){
     return value=='+' || value=='-' || value=='*' || value=='/';
 }
-
 for(var i=0;i<buttons.length;i++){
     buttons[i].addEventListener('click',function(){
         var value=this.getAttribute('data-value');
         var text = display.textContent.trim();
+        if(flag===false){
+            prev_display.textContent=text;
+            flag=true;
+        }       
         if(isOperator(value)){
             operator=value;
             operand1=parseFloat(text);
             display.textContent="";
+            prev_display.textContent+=operator;
         }else if(value=='ac'){
             display.textContent="";
+            prev_display.textContent="";
         }
         else if(value=="sign"){
             operand1=parseFloat(text);
-            operand1=-1 * operand1;
+            operand1=-1 * operand1;           
             display.textContent=operand1;
         }
         else if(value=='.'){
             if(text.length && !text.includes('.')){
                 display.textContent=text+"."
+                prev_display.textContent+=".";
             }
         }
         else if(value=='%'){
@@ -43,11 +50,13 @@ for(var i=0;i<buttons.length;i++){
                 display.textContent=result;
                 operand1=result;
                 operand2=null;
-                operator=null;
+                operator=null; 
+                flag=false;               
             }
         }
         else{
             display.textContent += value;
+            prev_display.textContent+=value;
         }
     });
 }

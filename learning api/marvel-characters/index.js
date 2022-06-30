747c9979aa8f245cc4367680026628d56ce72928
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-06-30 17:40:45 +0530

    updated little changes

--- learning api/marvel-characters/index.js
+++ learning api/marvel-characters/index.js
@@ -9,7 +9,7 @@ app.set('view engine','ejs');
 app.get('/' ,function(req,res){
   res.sendFile(__dirname+"/index.html")
 })
-
+app.use(express.static(__dirname + '/public'));
 
 app.post('/',function(req,res){
   request("https://gateway.marvel.com/v1/public/characters?ts=1&apikey=0e18886c150602cc9925a14586c7a549&hash=27d312585cb5f51e91b447e3d155a4d7",function(err,response,body)
